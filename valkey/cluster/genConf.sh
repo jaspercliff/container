e80ecf86108c26622e879a26e02a1fc3c232e7a7
jasper <2190320413@qq.com>
2026-03-09 13:15:11 +0800

    valkey cluster compose

--- valkey/cluster/genConf.sh
+++ valkey/cluster/genConf.sh
@@ -1,7 +1,7 @@
 for i in {1..6}; do
     export PORT=$((6380 + i - 1))
     export BPORT=$((PORT + 10000))
-    mkdir -p node-$i/{conf,data}
-    envsubst <valkey.conf.template >node-$i/conf/valkey.conf
+    mkdir -p {conf,data}/node-$i
+    envsubst <valkey.conf.template >conf/node-$i/valkey.conf
     echo "Node-$i: Valkey Port=$PORT, Bus Port=$BPORT"
 done
